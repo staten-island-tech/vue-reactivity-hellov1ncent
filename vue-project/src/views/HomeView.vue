d359a7be3b869c6b277bca2148e50a5e583d6209
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2024-02-08 10:13:47 -0500

    ff

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -12,41 +12,130 @@
 import DestCard from "@/components/icons/destcard.vue";
 const destinations = [
   {
-    name: "Venetian Renaissance",
-    International: true,
+    name: "Aj1 Lost and Found",
+    InStock: true,
     price: 1300,
-    img: "https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80&quot;",
+    img: "https://cdna.artstation.com/p/assets/images/images/035/193/174/large/mo-erges-nike-shoe-krbl.jpg?1614333257",
   },
   {
-    name: "Swiss Mountain Getaway",
-    International: true,
+    name: "Aj1 UNC",
+    Instock: true,
     price: 800,
     img: "https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80&quot;",
   },
   {
-    name: "Ibizan Coastal Vacation",
-    International: true,
+    name: "Jordan 4 Midnight Navy",
+    Instock: true,
     price: 1200,
     img: "https://images.unsplash.com/photo-1547138494-97041dec734b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80&quot;",
   },
   {
-    name: "Hawaiian Vacation",
-    International: false,
+    name: "Jordan 4 Military Black",
+    Instock: false,
     price: 900,
     img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80&quot;",
   },
   {
-    name: "Cascade Mountains",
-    International: false,
+    name: "Grey Fog Dunks",
+    Instock: false,
     price: 400,
     img: "https://images.unsplash.com/photo-1511497584788-876760111969?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80&quot;",
   },
   {
-    name: "Egyptian Exploration",
+    name: "Michigan State Dunks",
+    Instock: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Kobe Protro 6 All Star",
+    Instock: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Kobe Protro 6 Mambacita",
+    Instock: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Travis Scott Phantom",
+    Instock: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Kobe 5 Bruce Lee",
+    Instock: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Kobe Protro 6 Grinch",
+    Instock: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Travis Scott Reverse Mocha",
     International: true,
     price: 800,
     img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
   },
+  {
+    name: "Fragment X Travis Scott",
+    International: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "MSCHF RED BOOTS",
+    International: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Yeezy Slide Bone",
+    International: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Yeezy Slide Onyx",
+    International: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Jordan 3 White Cement Reimagined",
+    International: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Aj1 Retro High Dior",
+    International: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Aj1 Off-White Chicago",
+    International: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+  {
+    name: "Lightning Mcqueen Crocs",
+    International: true,
+    price: 800,
+    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+  },
+
+
+
+
+
 ];
 </script>
 
