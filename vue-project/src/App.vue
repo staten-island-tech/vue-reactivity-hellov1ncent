05efcbb3e1e144e9c2e32efeae061fbb6d3f7f82
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2024-02-15 11:05:55 -0500

    sada

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,22 +3,8 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
 
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+
 
   <RouterView />
 </template>
